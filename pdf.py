d4e0ee55a9961be3cf9abdc6eea7cdad13193c2d
OMER BIN ALI <omerbinali311@gmail.com>
2020-04-21 19:15:27 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,14 +1,14 @@
 import PyPDF2
+import sys
 
-template = PyPDF2.PdfFileReader(open('twopage.pdf', 'rb'))
-watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
-output = PyPDF2.PdfFileWriter()
+inputs=sys.argv[1:]
+def pdf_combiner(pdf_list):
+	merger=PyPDF2.PdfFileMerger()
+	for pdf in pdf_list:
+		print(pdf)
+		merger.append(pdf)
+	merger.write('super.pdf')
+	
 
-for i in range(template.getNumPages()):
-  page = template.getPage(i)
-  page.mergePage(watermark.getPage(0))
-  output.addPage(page)
-
-  with open('watermarked_output.pdf', 'wb') as file:
-    output.write(file)
+pdf_combiner(inputs)	
 
